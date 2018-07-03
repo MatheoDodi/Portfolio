146fa77caf23211afc2c5d1ca0fb524bd68675d1
Matthew Dodi <matthew.dodi@gmail.com>
2018-07-03 16:27:59 -0700

    Added scrolling animatio upon clicking the navigation links

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+window.addEventListener("scroll", Scroll);
+
+function Scroll() {
+	var navBar = document.getElementById("logo");
+	var ypos = window.pageYOffset;
+	var color =  document.querySelector(".navbar");
+	if (ypos > 550) {
+		navBar.style.width = "100%";
+		color.style.background = "#171a21";
+	} else {
+		navBar.style.width = "125%";
+		color.style.background = "#25282f";
+
+	}
+}
+
+$("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+      var link = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(link).offset().top
+      }, 800, function(){
+   
+      });
+    }
+  });
